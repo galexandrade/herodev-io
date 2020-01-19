6de07a1246124dd5a7f5895c7472ba672a5cfc32
Alex Andrade <g.alex.andrade@gmail.com>
2020-01-19 09:33:59 -0600

    Creating a subscribe form

---
+++ src/components/subscribe.js
@@ -0,0 +1,85 @@
+import React from "react"
+import styled from "styled-components"
+import Image from "gatsby-image"
+import { StaticQuery, graphql } from "gatsby"
+
+import { rhythm } from "../utils/typography"
+import Button from "./button"
+
+function Subscribe() {
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        return (
+          <Container>
+            <Row style={{ backgroundColor: "#F9FAFB" }}>
+              <h3 style={{ marginTop: 0 }}>Subscribe to the Newsletter</h3>
+              <p>Subscribe to get my latest content by email.</p>
+              <Image
+                fixed={data.emailIcon.childImageSharp.fixed}
+                alt="Email icon"
+                style={{
+                  marginRight: rhythm(1 / 2),
+                  marginBottom: 0,
+                  minWidth: 50,
+                }}
+              />
+            </Row>
+            <Row>
+              <form onSubmit={handleSubmit}>
+                <Input
+                  type="text"
+                  name="firstname"
+                  placeholder="Your first name"
+                  required
+                />
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="Your email"
+                  required
+                />
+                <Button>Subscribe</Button>
+              </form>
+            </Row>
+          </Container>
+        )
+      }}
+    />
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  box-shadow: 0 2px 15px 0 rgba(210, 214, 220, 0.5);
+  margin-bottom: 30px;
+`
+
+const Row = styled.div`
+  flex: 1;
+  padding: 30px;
+`
+
+const Input = styled.input`
+  padding: 10px;
+  margin-bottom: 20px;
+`
+
+const bioQuery = graphql`
+  query SubscribeQuery {
+    emailIcon: file(absolutePath: { regex: "/email.png/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
+
+export default Subscribe
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,6 +6,7 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
+import Subscribe from "../components/subscribe"
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -36,6 +37,7 @@ class BlogPostTemplate extends React.Component {
             marginBottom: rhythm(1),
           }}
         />
+        <Subscribe />
         <Bio />
 
         <ul
