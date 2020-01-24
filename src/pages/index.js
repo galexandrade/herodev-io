f0ebf0a2fae2aa0a503a98724f2dbd2e8bfc1200
Alex Andrade <g.alex.andrade@gmail.com>
2020-01-23 19:49:03 -0600

    Adding the new logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+        <img style={{ margin: 0 }} src="./assets/HeroDevLogo.svg" alt="Logo" />
         <h1>
           Hi devs{" "}
           <span role="img" aria-label="wave emoji">
